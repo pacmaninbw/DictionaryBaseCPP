691032b9c509835a2fa6be2b621c5ebab42ffdef
pacmaninbw <paul.chernick@chernicksw.com>
2024-10-11 05:32:21 -0700

    Added comments to document the constants as suggested in the code review.

--- src/tests/PerformanceTestGenerator/PerformanceTestGenerator.h
+++ src/tests/PerformanceTestGenerator/PerformanceTestGenerator.h
@@ -14,7 +14,6 @@ public:
     bool generateAllPerformaneTests() noexcept;
 
 private:
-
     bool genTestDataPairs(std::size_t testSize, std::size_t tabCount) const noexcept;
     bool genTestDataStructVector(std::size_t testSize) const noexcept;
     std::string genTestFunc(std::size_t testSize) const noexcept;
@@ -24,13 +23,26 @@ private:
     bool generateStringVector(std::size_t testSize) const noexcept;
     std::string reconstructCommandLine() noexcept;
 
-
+/*
+ * The first 3 constants are used for enumeration code generation. While C++ enums
+ * start at zero, the GenericDictionary class assumes that an enum with the value
+ * of zero is an invalid, valid enums values are 1 through test size.
+ * 
+ * GD stands for GenericDicionary
+ */
     const std::size_t MinEnumVal = 1;
-    const char* enumTemplate{"GD_PERFORMANCE_TEST_VALUE_"};
+    const char* enumTemplate{"GD_PerfTestEnumValue_"};
     const char* enumName{"GDPerformanceTestEnum"};
-    const char* testStrTemplate{"Performance Test String "};
+/*
+ * The following 2 constants are for testing the strings associated with the enums.
+ */
+    const char* testStrTemplate{"GD Perf Test String "};
     const char* testStrVectorName{"GDPerformanceTestStrings"};
-    std::vector<std::size_t> testValues;
+
+/*
+ * Private variables
+ */
+    std::vector<std::size_t> testValues;    // Each item in in the vector is a test size to generate.
     std::string progName;
     std::string version;
     bool useCout = true;
