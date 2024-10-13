c2c85e3616e1e6a1a3a1663c8510f76dcd31fa47
pacmaninbw <paul.chernick@chernicksw.com>
2024-10-13 12:17:52 -0700

    Standardized the way numeric args are handled. Removed is_number() now catch std::stoi errors to handle bad input. Made error reporting a little more generic.

--- src/tests/PerformanceTestGenerator/TestParameters.cpp
+++ src/tests/PerformanceTestGenerator/TestParameters.cpp
@@ -11,19 +11,29 @@ static std::string simplify_name(char *path)
 }
 
 TestParameters::TestParameters(int argc, char *argv[])
-: version{"2.0.0"},
+: progName{simplify_name(argv[0])},
+  version{"2.0.0"},
   args{argv + 1, argv + argc}
 {
-    progName = simplify_name(argv[0]);
+    /*
+     * common parts of help messages.
+     */
+    const std::string countMsg("test count: The number of tests to generate, ");
+    const std::string sizeMsg("test size: The number of enum values in this test to generate, ");
+    const std::string outPutFileMsg("The name of the output file to be generated");
+
+/*
+ * List the preferred longer flags first. This will make error messages easier to build.
+ */
     testInfo = 
     {
-        {"--output-file FILENAME", ArgTypes::OutPutFile, "The name of the output file to be generated"},
-        {"--test-count INTEGER", ArgTypes::TestCount, "\ttest count: integer value between 1 and 10, the number of tests to generate"},
-        {"--test-size INTEGER", ArgTypes::TestSize, "\ttest size: integer value between 1 and 524288 The number of enum values in this test to generat"},
-        {"-c INTEGER", ArgTypes::TestCount, "\t\ttest count: integer value between 1 and 10, the number of tests to generate"},
-        {"-s INTEGER", ArgTypes::TestSize, "\t\ttest size: integer value between 1 and 524288 The number of enum values in this test to generate"},
-        {"-f FILENAME", ArgTypes::OutPutFile, "\t\tThe name of the output file to be generated"},
-        {"--help", ArgTypes::Help, "\t\tPrint this help message."}
+        {"--output-file", ArgTypes::OutPutFile, outPutFileMsg, {0, 0}, 1},
+        {"--test-count", ArgTypes::TestCount, countMsg, countRange, 1},
+        {"--test-size", ArgTypes::TestSize, sizeMsg, sizeRange, 1},
+        {"-c", ArgTypes::TestCount, countMsg, countRange, 2},
+        {"-s", ArgTypes::TestSize, sizeMsg, sizeRange, 2},
+        {"-f", ArgTypes::OutPutFile, outPutFileMsg, {0, 0}, 2},
+        {"--help", ArgTypes::Help, "Print this help message.", {0, 0}, 2}
     };
 }
 
@@ -40,96 +50,6 @@ bool TestParameters::getTestParameters() noexcept
     }
 
     return verifyTestCount();
-}
-
-bool TestParameters::verifyTestCount() const noexcept
-{
-    if (testCount > 0 && testCount != testValues.size())
-    {
-        std::cerr << "--test-count value (" << std::to_string(testCount) <<
-            ") does not agree with number of test sizes input (" <<
-            std::to_string(testValues.size()) << ")\n\n\n";
-        usage();
-        return false;
-    }
-
-    return true;
-}
-
-void TestParameters::usage() const noexcept
-{
-    std::cerr << progName << " " << version << "\n";
-    std::cerr << "Usage: " << progName <<
-        " [ [--output-file FILENAME] --test-count INTEGER --test-size INTEGER"
-        " [--test-size INTEGER]*]\n";
-    for (auto tad: testInfo)
-    {
-        std::cerr << tad.argName << ": " << tad.helpMessage << "\n";
-    }
-
-    std::cerr << "\nBy default the output of the program goes to the screen. The user can over ride this\n";
-    std::cerr << "by either redirecting the output to a file or specifying the output file with the\n";
-    std::cerr << "--output-file flag. If the user doesn't provide any arguments on the command line\n";
-    std::cerr << "the program will request the number of tests and the test sizes\n";
-}
-
-void TestParameters::getUserInput() noexcept
-{
-    std::size_t testCount = 
-        safeUserNumericInput("Enter the number of tests to generate", MIN_TEST_COUNT, MAX_TEST_COUNT);
-
-    for (std::size_t lcount = 0; lcount < testCount; ++lcount)
-    {
-        testValues.push_back(
-            safeUserNumericInput("Enter the number of enums to generate for this test", MIN_TEST_VALUE, MAX_TEST_VALUE)
-        );
-    }
-}
-
-std::size_t TestParameters::safeUserNumericInput(std::string message, std::size_t minValue, std::size_t maxValue)
-{
-    std::size_t safeNumber = 0;
-    std::string safeInput;
-
-    do {
-        std::cout << message << " [" << minValue << "," << maxValue << "]\n>>";
-        std::cin >> safeInput;
-        safeNumber = safeNumericConversion(safeInput, minValue, maxValue);
-    } while (safeNumber < 1);
-
-    return safeNumber;
-}
-
-bool TestParameters::is_number(const std::string &s)
-{
-    return !s.empty() && std::find_if(s.begin(), 
-        s.end(), [](unsigned char c) { return !std::isdigit(c); }) == s.end();
-}
-
-std::size_t TestParameters::safeNumericConversion(std::string userInput, std::size_t minValue, std::size_t maxValue)
-{
-    if (is_number(userInput))
-    {
-        std::size_t safeNumber = std::stoi(userInput);
-        if (safeNumber >= minValue || safeNumber <= maxValue)
-        {
-            return safeNumber;
-        }
-    }
-
-    return 0;
-}
-
-bool TestParameters::hasRequiredArgument(std::size_t currentArg, std::string argName, std::string argType) const noexcept
-{
-    if (currentArg >= args.size() || args[currentArg][0] == '-') 
-    {
-        std::cerr << "Missing required " << argType << " for " << argName << "\n\n";
-        usage();
-        return false;
-    }
-
-    return true;
 }
 
 bool TestParameters::processArguments() noexcept
@@ -155,7 +75,6 @@ bool TestParameters::processArguments() noexcept
                     ++currentArg;
                     if (!processTestCount(currentArg))
                     {
-                        usage();
                         return false;
                     }
                     break;
@@ -180,31 +99,162 @@ bool TestParameters::processArguments() noexcept
     return true;
 }
 
-// Need to find a way to use one function for processTestSize and processTestCount
+bool TestParameters::verifyTestCount() const noexcept
+{
+    if (testCount > 0 && testCount != testValues.size())
+    {
+        std::cerr << "--test-count value (" << std::to_string(testCount) <<
+            ") does not agree with number of test sizes input (" <<
+            std::to_string(testValues.size()) << ")\n\n\n";
+        usage();
+        return false;
+    }
+
+    return true;
+}
+
+void TestParameters::usage() const noexcept
+{
+    std::cerr << progName << " " << version << "\n";
+    std::cerr << "Usage: " << progName <<
+        " [ [--output-file FILENAME] --test-count INTEGER --test-size INTEGER"
+        " [--test-size INTEGER]*]\n";
+
+    printArgList();
+    printMoreUsageInfo();
+
+}
+
+void TestParameters::printArgList() const noexcept
+{
+    for (auto tad: testInfo)
+    {
+        /*
+         * Work around for the fact that g++-13 can't be installed on Ubuntu 22.04
+         * so std::format() is not supported.
+         */
+        std::string legalValues("");
+        if (tad.range.min > 0)
+        {
+            legalValues = "integer value between " + std::to_string(tad.range.min) +
+                " and " + std::to_string(tad.range.max);
+        }
+        std::cerr << tad.argName << ":" << std::string(tad.tabCount, '\t') <<
+            tad.helpMessage << legalValues << "\n";
+    }
+}
+
+void TestParameters::printMoreUsageInfo() const noexcept
+{
+    std::cerr << "\nBy default the output of the program goes to the screen. The user can over ride this\n";
+    std::cerr << "by either redirecting the output to a file or specifying the output file with the\n";
+    std::cerr << "--output-file flag. If the user doesn't provide any arguments on the command line\n";
+    std::cerr << "the program will request the number of tests and the test sizes\n";
+}
+
+void TestParameters::getUserInput() noexcept
+{
+    std::size_t testCount = 
+        safeUserNumericInput("Enter the number of tests to generate",  countRange);
+
+    for (std::size_t lcount = 0; lcount < testCount; ++lcount)
+    {
+        testValues.push_back(
+            safeUserNumericInput("Enter the number of enums to generate for this test", sizeRange)
+        );
+    }
+}
+
+std::size_t TestParameters::safeUserNumericInput(std::string message, TestRange testRange)
+{
+    std::size_t safeNumber = 0;
+    std::string safeInput;
+
+    do {
+        std::cout << message << " [" << testRange.min << "," << testRange.max << "]\n>>";
+        std::cin >> safeInput;
+        safeNumber = safeNumericConversion(safeInput, testRange);
+    } while (safeNumber < 1);
+
+    return safeNumber;
+}
+
+std::size_t TestParameters::safeNumericConversion(std::string userInput, TestRange testRange)
+{
+    std::size_t safeNumber = 0;
+    
+    try
+    {
+        safeNumber = static_cast<std::size_t>(std::stoi(userInput, nullptr, 16));
+    }
+    catch(std::invalid_argument& e)
+    {
+        return 0;
+    }
+    catch (std::out_of_range& e)
+    {
+        return 0;
+    }
+    
+    return ((safeNumber >= testRange.min && safeNumber <= testRange.max)? safeNumber : 0);
+}
+
+bool TestParameters::hasRequiredArgument(std::size_t currentArg, ArgTypes argType) noexcept
+{
+    if (currentArg >= args.size() || args[currentArg][0] == '-') 
+    {
+        std::cerr << "Missing required " << buildErrorMessageFromArg(argType) << "\n\n";
+        usage();
+        return false;
+    }
+
+    return true;
+}
+
 bool TestParameters::processTestSize(std::size_t currentArg) noexcept
 {
-    if (hasRequiredArgument(currentArg, "--test-size [-s]", "integer value"))
+    std::size_t input = processNumericArg(currentArg, ArgTypes::TestSize, sizeRange);
+    if (input >= sizeRange.min)
     {
-        std::size_t input = safeNumericConversion(args[currentArg], MIN_TEST_VALUE, MAX_TEST_VALUE);
-        if (input > MIN_TEST_VALUE)
-        {
-            testValues.push_back(input);
-            return true;
-        }
-        else
-        {
-            std::cerr << "Error in command line: --test-size [-s] value " <<
-                args[currentArg] << " out of range[" << MIN_TEST_VALUE << "," << MAX_TEST_VALUE << "]\n\n";
-            usage();
-        }
+        testValues.push_back(input);
+        return true;
     }
 
     return false;
 }
 
+std::size_t TestParameters::processNumericArg(std::size_t currentArg, ArgTypes argType, TestRange argRange) noexcept
+{
+    std::size_t input = 0;
+
+    if (hasRequiredArgument(currentArg, argType))
+    {
+        input = safeNumericConversion(args[currentArg], argRange);
+        if (input > argRange.min)
+        {
+            return input;
+        }
+        else
+        {
+            std::cerr << "Error in command line: " << buildArgsString(argType) <<" value " <<
+                args[currentArg] << " out of range[" << argRange.min << "," << argRange.max << "]\n\n";
+            usage();
+        }
+    }
+
+    return 0;
+}
+
+bool TestParameters::processTestCount(std::size_t currentArg) noexcept
+{
+    testCount = processNumericArg(currentArg, ArgTypes::TestCount, countRange);
+
+    return testCount >= countRange.min;
+}
+
 bool TestParameters::processOutputFile(std::size_t currentArg) noexcept
 {
-    if (hasRequiredArgument(currentArg, "--output-file [-s]", "output file name"))
+    if (hasRequiredArgument(currentArg, ArgTypes::OutPutFile))
     {
         outPutFile = args[currentArg];
         useCout = false;
@@ -214,30 +264,62 @@ bool TestParameters::processOutputFile(std::size_t currentArg) noexcept
     return false;
 }
 
-bool TestParameters::processTestCount(std::size_t currentArg) noexcept
-{
-    if (hasRequiredArgument(currentArg, "---test-count [-c]", "integer value"))
-    {
-        std::size_t input = safeNumericConversion(args[currentArg], MIN_TEST_COUNT, MAX_TEST_COUNT);
-        if (input > MIN_TEST_COUNT)
-        {
-            testCount = input;
-            return true;
-        }
-        else
-        {
-            std::cerr << "Error in command line: --test-count [-c] value " <<
-                args[currentArg] << " out of range[" << MIN_TEST_COUNT << "," << MAX_TEST_COUNT << "]\n\n";
-            usage();
-        }
-    }
-
-    return false;}
-
 TestParameters::TestArgData TestParameters::getArgType(std::string flagStr) noexcept
 {
     auto argData = std::find_if(testInfo.begin(), testInfo.end(),
         [&flagStr](TestArgData &flagData) {return (flagData.argName == flagStr);});
 
-    return (argData != testInfo.end()? *argData : TestArgData{"error", ArgTypes::ErrorArgType, "Unknown switch"});
+    return (argData != testInfo.end()? *argData : TestArgData{"error", ArgTypes::ErrorArgType, "Unknown switch", 0, 0, 0});
+}
+
+std::vector<std::string> TestParameters::getAllFlagNamesForErrorMessage(ArgTypes argType) noexcept
+{
+    std::vector<std::string> allArgNames;
+
+    for (auto ti: testInfo)
+    {
+        if (ti.argType == argType)
+        {
+            allArgNames.push_back(ti.argName);
+        }
+    }
+
+    return allArgNames;
+}
+
+/*
+ * Assumes argType is a type that requires a second argument.
+ */
+std::string TestParameters::getTypeNameFromArg(ArgTypes argType) noexcept
+{
+    if (argType == ArgTypes::OutPutFile)
+    {
+        return "output file name";
+    }
+
+    return "integer value";
+}
+
+std::string TestParameters::buildErrorMessageFromArg(ArgTypes argType) noexcept
+{
+    std::string errorMessage(getTypeNameFromArg(argType));
+    errorMessage += " for ";
+    errorMessage += buildArgsString(argType);
+
+    return errorMessage;
+}
+
+std::string TestParameters::buildArgsString(ArgTypes argType) noexcept
+{
+    std::string argsString;
+    std::vector<std::string> argNames = getAllFlagNamesForErrorMessage(argType);
+    bool useBrackets = false;
+
+    for (auto argName: argNames)
+    {
+        argsString += useBrackets? " [" + argName + "]" : argName;
+        useBrackets = true;
+    }
+
+    return argsString;
 }
--- src/tests/PerformanceTestGenerator/TestParameters.h
+++ src/tests/PerformanceTestGenerator/TestParameters.h
@@ -2,8 +2,16 @@
 #define TESTPARAMETERS_H_
 
 #include <string>
+#include <utility>
 #include <vector>
 
+/*
+ * Get the test generation parameters. For output the program defaults to
+ * standard out. There are no defaults for the number of tests to generate
+ * or the size of the tests to generate. If the user does not enter this
+ * information on the command line the program will ask the user to provide
+ * input.
+ */
 class TestParameters
 {
 public:
@@ -16,27 +24,6 @@ public:
     std::string outPutFile;
 
 private:
-    bool verifyTestCount() const noexcept;
-    void usage() const noexcept;
-    void getUserInput() noexcept;
-    std::size_t safeUserNumericInput(std::string message, std::size_t minValue, std::size_t maxValue);
-    bool is_number(const std::string& s);
-    std::size_t safeNumericConversion(std::string userInput, std::size_t minValue, std::size_t maxValue);
-    bool hasRequiredArgument(std::size_t currentArg, std::string argName, std::string argType) const noexcept;
-    bool processArguments() noexcept;
-    bool processTestSize(std::size_t currentArg) noexcept;
-    bool processOutputFile(std::size_t currentArg) noexcept;
-    bool processTestCount(std::size_t currentArg) noexcept;
-
-
-    std::vector<std::string> args;
-    std::size_t testCount = 0;
-
-    const std::size_t MIN_TEST_COUNT = 1;
-    const std::size_t MAX_TEST_COUNT = 10;
-    const std::size_t MIN_TEST_VALUE = 1;
-    const std::size_t MAX_TEST_VALUE = (1024 * 512);
-
     enum class ArgTypes
     {
         TestSize,
@@ -45,14 +32,58 @@ private:
         Help,
         ErrorArgType
     };
+
+    struct TestRange
+    {
+        std::size_t min;
+        std::size_t max;
+    };
+
     struct TestArgData
     {
         std::string argName;
         ArgTypes argType;
         std::string helpMessage;
+        TestRange range;
+        std::size_t tabCount;
     };
-    std::vector<TestArgData> testInfo;
+
+    bool verifyTestCount() const noexcept;
+    void usage() const noexcept;
+    void printArgList() const noexcept;
+    void printMoreUsageInfo() const noexcept;
+    void getUserInput() noexcept;
+    std::size_t safeUserNumericInput(std::string message, TestRange testRange);
+    std::size_t safeNumericConversion(std::string userInput, TestRange testRange);
+    bool hasRequiredArgument(std::size_t currentArg, ArgTypes argType) noexcept;
+    bool processArguments() noexcept;
+    bool processOutputFile(std::size_t currentArg) noexcept;
+    bool processTestCount(std::size_t currentArg) noexcept;
+    bool processTestSize(std::size_t currentArg) noexcept;
+    std::size_t processNumericArg(std::size_t currentArg, ArgTypes argType, TestRange argRange) noexcept;
     TestArgData getArgType(std::string flagStr) noexcept;
+    std::vector<std::string> getAllFlagNamesForErrorMessage(ArgTypes argType) noexcept;
+    std::string getTypeNameFromArg(ArgTypes argType) noexcept;
+    std::string buildErrorMessageFromArg(ArgTypes argType) noexcept;
+    std::string buildArgsString(ArgTypes argType) noexcept;
+
+/*
+ * These constant are used for error checking during user input or
+ * command line parsing. The numbers are somewhat arbitrary. Due to
+ * the limitations of the compilers on the development machine the
+ * maximum test size is limited to 512K.
+ */
+    const std::size_t MinTestCount = 1;
+    const std::size_t MaxTestCount = 10;
+    const std::size_t MinTestSize = 1;
+    const std::size_t MaxTestSize = (1024 * 512); // 512K
+
+    const TestRange sizeRange{MinTestSize, MaxTestSize};
+    const TestRange countRange{MinTestCount, MaxTestCount};
+
+    std::vector<std::string> args;
+    std::size_t testCount = 0;
+    std::vector<TestArgData> testInfo;
 };
 
 #endif // TESTPARAMETERS_H_
